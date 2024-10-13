e69115d6fcb5d611738929a0790227eb82d59131
ReneProgramming <renehernandez350@yahoo.com>
2024-10-12 22:17:55 -0700

    All new users get fan role

--- AnimeFanWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ AnimeFanWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using AnimeFanWeb.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -124,6 +125,9 @@ namespace AnimeFanWeb.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+                    // Adds new accounts to the fan role
+                    await _userManager.AddToRoleAsync(user, IdentityHelper.Fan);
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
