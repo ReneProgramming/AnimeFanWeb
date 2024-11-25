f41cf87c04567a7d2ebf99c99f346302d320559c
ReneProgramming <renehernandez350@yahoo.com>
2024-11-24 23:42:09 -0800

    Created view model for the create and edit views

--- AnimeFanWeb/Models/AnimeList.cs
+++ AnimeFanWeb/Models/AnimeList.cs
@@ -20,5 +20,56 @@ namespace AnimeFanWeb.Models
         public DateOnly? EndDate { get; set; }
     }
 
+    public class AnimeListCreateViewModel
+    {
+        public string Title { get; set; }
+
+        public string Type { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Description { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly? EndDate { get; set }
+
+    }
+
+    public class AnimeListEditViewModel
+    { 
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Type { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Description { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly? EndDate { get;set; }
+    }
+
+    public class AnimeListIndexViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Anime Title")]
+        public string Title { get; set; }
+
+        [Display(Name ="Show/Movie/OVA")]
+        public string Type { get; set; }
+
+        public string Genre { get; set; }
+
+        [Display(Name ="Date Aired")]
+        public DateOnly StartDate { get; set; }
+
+        [Display(Name ="Date Ended")]
+        public DateOnly? EndDate { get; set; }
+    }
 
 }
