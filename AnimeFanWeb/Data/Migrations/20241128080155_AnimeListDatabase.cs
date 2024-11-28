aa64460ef1ddb5d624f78668dca37b42f80aa9af
ReneProgramming <renehernandez350@yahoo.com>
2024-11-28 00:05:28 -0800

    Index view model and create view model implemented into anime list controller

---
+++ AnimeFanWeb/Data/Migrations/20241128080155_AnimeListDatabase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimeFanWeb.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AnimeListDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AnimeList",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AnimeList");
+        }
+    }
+}
--- AnimeFanWeb/Models/AnimeList.cs
+++ AnimeFanWeb/Models/AnimeList.cs
@@ -15,6 +15,8 @@ namespace AnimeFanWeb.Models
 
         public string Genre { get; set; }
 
+        public string Description { get; set; }
+
         public DateOnly StartDate { get; set; }
 
         public DateOnly? EndDate { get; set; }
@@ -55,21 +57,21 @@ namespace AnimeFanWeb.Models
 
     public class AnimeListIndexViewModel
     {
-        public int Id { get; set; }
+        public int AnimeListId { get; set; }
 
         [Display(Name = "Anime Title")]
-        public string Title { get; set; }
+        public string AnimeTitle { get; set; }
 
         [Display(Name ="Show/Movie/OVA")]
-        public string Type { get; set; }
+        public string AnimeType { get; set; }
 
-        public string Genre { get; set; }
+        public string AnimeGenre { get; set; }
 
         [Display(Name ="Date Aired")]
-        public DateOnly StartDate { get; set; }
+        public DateOnly AnimeStartDate { get; set; }
 
         [Display(Name ="Date Ended")]
-        public DateOnly? EndDate { get; set; }
+        public DateOnly? AnimeEndDate { get; set; }
     }
 
 }
