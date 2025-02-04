b94e655c541617f79488de26b3e0d17a417edee4
ReneProgramming <renehernandez350@yahoo.com>
2025-02-03 23:58:46 -0800

    Implemented a beginning watch list

---
+++ AnimeFanWeb/Models/UserAnime.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimeFanWeb.Models
+{
+    public class UserAnime
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public IdentityUser User { get; set; }
+
+        [Required]
+        public int AnimeListId { get; set; }
+
+        [ForeignKey("AnimeListId")]
+        public AnimeList Anime { get; set; }
+
+        [Required]
+        public WatchStatus Status { get; set; }
+    }
+
+    public enum WatchStatus
+    {
+        ToWatch,
+        Watching,
+        Done
+    }
+}
